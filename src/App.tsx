5e00fb202b3b5906de55000723ebd875eb87af90
heroK12 <koudai2517@gmail.com>
2022-08-22 22:23:07 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { faker } from '@faker-js/faker';
 
-function App() {
+export const App = () => {
+    type TodoType = {
+      userId: number;
+      id: number;
+      title: string;
+      completed: boolean;
+    };
+    
+    const [todos, setTodos] = useState<Array<TodoType>>([]);
+    
+    //axiosで取得したデータに上記で定義したTodoType型を指定する
+    const onClickFetchData = () => {
+      axios
+        .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+        .then((res) => {
+          setTodos(res.data);
+        });
+    };
+
+    // 初期化が必要
+    let dummyData: Array<TodoType> = [];
+
+    // ダミーデータを作る
+    let setDummyData = (): void => {
+      let createDummyData = {
+        userId: faker.datatype.number(),
+        id: faker.datatype.number(),
+        title: faker.company.bsNoun(),
+        completed: faker.datatype.boolean(),
+      }
+      // console.log(createDummyData);
+      // console.log(dummyData);
+      // 配列に入れる
+      dummyData.push(createDummyData);
+    };
+    // ループでダミーデータを配列に入れる
+    const onClickSetData = () => {
+      for(let i = 0; i < 5; i++){
+        setDummyData()
+      }
+      setTodos(dummyData);
+    }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickSetData}>データ取得</button>
+      {todos.map((data,index)=><p key={index}>{data.id}</p>)}
     </div>
   );
 }
